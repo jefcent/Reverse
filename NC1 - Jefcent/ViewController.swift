9a56d0d5660f61a2aa6bd52282d8893ff18b9a0c
Jefcent <jjapherlich.jj@gmail.com>
2022-04-28 15:08:01 +0700

    Initial Commit

---
+++ NC1 - Jefcent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NC1 - Jefcent
+//
+//  Created by Jefcent Japherlich on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
